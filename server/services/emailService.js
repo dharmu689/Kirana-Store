171da62b98d65ca0ecfedeb39746236809269145
Dharmu Kumar <dharmukumar1622003@gmail.com>
2026-03-12 02:15:32 +0530

    Update emailService.js

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -9,9 +9,7 @@ const nodemailer = require('nodemailer');
 const sendVendorReorderEmail = async (vendorEmail, reorderData, pdfPath) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // true for 465, false for other ports
+            service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
